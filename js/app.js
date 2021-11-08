50a8a68980215850a98519e22056634b1ddf201c
pgm-dionvanh <dionvanh@student.arteveldehs.be>
2021-11-08 19:39:13 +0100

    Fixes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
 (() => {
+    const app = {
+        loopNavigation() {
+            console.log("Create navigation")
+            return NavigationArray.map((item) => {
+                if(item.type === "internal"){
+                    if(item.name === "koop tickets")
+                        return $('.navmenu').append($(`<li class="nav__item lastchild"><a class="lastchild" href="${item.link}" target="_blank">${item.name}</a></li>`)) /* todo external class */
+                    else
+                        return $('.navmenu').append($(`<li class="nav__item"><a href="${item.link}" target="_blank">${item.name}</a></li>`)) /* todo external class */
+                } else {
+                    return $('.navmenu').append($(`<li class="nav__item"><a href="${item.link}" target="_blank">${item.name}</a></li>`)) /* todo external class */
+                }
+            }).join('')
+        },
+        loopLineUp() {
+            console.log("Create LineUp");
+            return LineUpArray.map((item) => {
+                const ArtistPlayDay = new Date(item.from).getDay();
 
-/* Navigation array */
-const NavigationArray = [
-    {
-        name: "Test",
-        link: "hallo.com",
-        type: "internal"
-    },
-    {
-        name: "Test",
-        link: "hallo.com",
-        type: "internal"
-    }
-];
-
-const app = {
-    loopNavigation() {
-        for (item of NavigationArray){
-            /* Usage of jquery */
-            $('#NavigationList').append($(`<li><a>${item.name}</a></li>`));
+                return $('.test').append($(`<li>hi</li>`));
+            });
+        },
+        Initialize() {
+            console.log("Initialize app");
+            this.loopNavigation();
+            this.loopLineUp();
         }
-    },
-    
-    Initialize() {
-        this.loopNavigation();
     }
-}
 
-app.Initialize(); /* Initialize app */
+    app.Initialize(); /* Initialize app */
 
 
 
---
+++ js/data.js
@@ -0,0 +1,81 @@
+/* Navigation array */
+const NavigationArray = [
+    {
+        name: "home",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "line up",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "tickets",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "praktisch",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "camping",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "history",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "rwtv",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "nmw",
+        link: "hallo.com",
+        type: "internal"
+    },
+    {
+        name: "koop tickets",
+        link: "hallo.com",
+        type: "internal"
+    }
+];
+
+/* Lineup array */
+
+const LineUpArray = [
+    {
+        id: "",
+        artist: {
+            name: "hi",
+            synopsis: "",
+            social: {
+                website: "",
+                facebook: "",
+                instagram: ""
+            },
+            picture: {
+                small: "",
+                large: ""
+            },
+            media: {
+                type: "",
+                sourceId: ""
+            },
+            place: {
+                name: ""
+            },
+            from: 12,
+            to: 12,
+            createdAt: 12,
+            modifiedAt: 12,
+            isHeadliner: false
+        }
+    }
+]
